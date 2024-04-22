ab5a3935980a3849e2266f1a6477f1334eb7e69f
Victor Pakholkov <VictorPakholkov@outlook.com>
2024-04-22 13:53:42 +0300

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -1,107 +1,74 @@
-from torch import nn
-
-model_url = "https://download.pytorch.org/models/resnet34-333f7ec4.pth"
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, layers):
-        super().__init__()
-        self.in_channels = 64
-        self.conv1 = nn.Conv2d(
-            3, 64, kernel_size=7, stride=2, padding=3, bias=False
-        )  # noqa
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight, mode="fan_out", nonlinearity="relu"
-                )  # noqa
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, out_channels, num_block, stride=1):
-        downsample = None
-        if stride != 1 or self.in_channels != out_channels * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(
-                    self.in_channels,
-                    out_channels * block.expansion,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(out_channels * block.expansion),
-            )
-        layers = []
-        layers.append(block(self.in_channels, out_channels, stride, downsample))  # noqa
-        self.in_channels = out_channels * block.expansion
-        for _ in range(1, num_block):
-            layers.append(block(self.in_channels, out_channels))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        C1 = self.conv1(x)
-        C1 = self.bn1(C1)
-        C1 = self.relu(C1)
-        C1 = self.maxpool(C1)
-        C2 = self.layer1(C1)
-        C3 = self.layer2(C2)
-        C4 = self.layer3(C3)
-        C5 = self.layer4(C4)
-        return C5
+import torch
+import torch.nn as nn
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
+class YoloBackbone(nn.Module):
+	def __init__(self):
+		super(YoloBackbone, self).__init__()
+		conv1 = nn.Sequential(
+			nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+			# nn.BatchNorm2d(64),
+			nn.LeakyReLU(0.1, inplace=True)
+		)
+		pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+		conv2 = nn.Sequential(
+			nn.Conv2d(64, 192, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(192),
+			nn.LeakyReLU(0.1, inplace=True)
+		)
+		pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+		conv3 = nn.Sequential(
+			nn.Conv2d(192, 128, kernel_size=1),
+			# nn.BatchNorm2d(128),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(128, 256, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(256),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(256, 256, kernel_size=1),
+			# nn.BatchNorm2d(256),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(256, 512, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(512),
+			nn.LeakyReLU(0.1, inplace=True)
+		)
+		pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super().__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=3,
-            stride=stride,
-            padding=1,
-            bias=False,
-        )
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(
-            out_channels,
-            out_channels,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False,  # noqa
-        )
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
+		conv4_part = nn.Sequential(
+			nn.Conv2d(512, 256, kernel_size=1),
+			# nn.BatchNorm2d(256),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(256, 512, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(512),
+			nn.LeakyReLU(0.1, inplace=True)
+		)
+		conv4_modules = []
+		for _ in range(4):
+			conv4_modules.append(conv4_part)
+		conv4 = nn.Sequential(
+			*conv4_modules,
+			nn.Conv2d(512, 512, kernel_size=1),
+			# nn.BatchNorm2d(512),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(1024),
+			nn.LeakyReLU(0.1, inplace=True)
+		)
+		pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+		conv5 = nn.Sequential(
+			nn.Conv2d(1024, 512, kernel_size=1),
+			# nn.BatchNorm2d(512),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(1024),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(1024, 512, kernel_size=1),
+			# nn.BatchNorm2d(512),
+			nn.LeakyReLU(0.1, inplace=True),
+			nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+			# nn.BatchNorm2d(1024),
+			nn.LeakyReLU(0.1, inplace=True)
+		)
+		self.net = nn.Sequential(conv1, pool1, conv2, pool2, conv3, pool3, conv4, pool4, conv5)
 
-    def forward(self, x):
-        identity = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-        return out
-
-
-def backbone_create():
-    feat_dims = 512
-    model = ResNet(BasicBlock, [3, 4, 6, 3])
-    return model, feat_dims
+	def forward(self, X):
+		return self.net(X)
