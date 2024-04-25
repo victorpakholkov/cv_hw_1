ee06262a870da6d448d61dc4be6d220dab5224f1
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-04-25 16:40:56 +0300

    add collate func to form batches

--- dataloader/transform.py
+++ dataloader/transform.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision
 import fiftyone
+from PIL import Image
+import fiftyone.utils.coco as fouc
+from itertools import product
 
 class FiftyOneTorchDataset(torch.utils.data.Dataset):
     """A class to construct a PyTorch dataset from a FiftyOne dataset.
@@ -44,6 +47,7 @@ class FiftyOneTorchDataset(torch.utils.data.Dataset):
         sample = self.samples[img_path]
         metadata = sample.metadata
         img = Image.open(img_path).convert("RGB")
+        width, height = img.size
 
         boxes = []
         labels = []
@@ -56,7 +60,7 @@ class FiftyOneTorchDataset(torch.utils.data.Dataset):
                 det, metadata, category_id=category_id,
             )
             x, y, w, h = coco_obj.bbox
-            boxes.append([x, y, x + w, y + h])
+            boxes.append([(x + w / 2) / width, (y + h / 2) / height, w / width, h / height]) # normalized (xc, yc, w, h)
             labels.append(coco_obj.category_id)
             area.append(coco_obj.area)
             iscrowd.append(coco_obj.iscrowd)
@@ -86,3 +90,47 @@ class Transformtsz:
         image = torchvision.transforms.functional.resize(image, self.resize)
         image = torchvision.transforms.functional.to_tensor(image)
         return image, boxes
+
+def collate(batch, grid_size=7, n_classes=80):
+    images = []
+    gt = []
+    for item in batch:
+        images.append(item[0])
+
+        fmap = torch.zeros(1, grid_size, grid_size, 5*2+n_classes)
+        bboxes = item[1]["boxes"]
+        labels = item[1]["labels"]
+
+        used_col_row = {(r, c): 0 for r,c in list(product(range(7), repeat=2))}
+        for bbox, label in zip(bboxes, labels):
+            col = int(bbox[1] * grid_size)
+            row = int(bbox[0] * grid_size)
+            cell_size = 1 / grid_size
+            row_interval = (cell_size*row, cell_size*(row+1))
+            col_interval = (cell_size*col, cell_size*(col+1))
+
+            # if more than 2 bboxes in one cell then skip
+            if used_col_row[(row, col)] == 2:
+                continue
+
+            used_col_row[(row, col)] += 1
+
+            if used_col_row[(row, col)] == 1:
+                # bbox center coords relative to grid cell
+                fmap[0, row, col, 0]  = (bbox[0] - row_interval[0]) / (row_interval[1] - row_interval[0])
+                fmap[0, row, col, 1]  = (bbox[1] - col_interval[0]) / (col_interval[1] - col_interval[0])
+                fmap[0, row, col, 2:4]  = bbox[2:] # bbox w and h relative to image size
+                fmap[0, row, col, 4] = 1 # confindece
+            elif used_col_row[(row, col)] == 2:
+                # bbox center coords relative to grid cell
+                fmap[0, row, col, 5]  = (bbox[0] - row_interval[0]) / (row_interval[1] - row_interval[0])
+                fmap[0, row, col, 6]  = (bbox[1] - col_interval[0]) / (col_interval[1] - col_interval[0])
+                fmap[0, row, col, 7:9]  = bbox[2:] # bbox w and h relative to image size
+                fmap[0, row, col, 9] = 1 # confindece
+            # set classes probabilities
+            fmap[0, row, col, label + 10] = 1
+        gt.append(fmap)
+    
+    images = torch.cat(images, 0)
+    detections = torch.cat(gt, 0)
+    return (images, detections)
