04a01baa1017c5cd85f97aaaae8d7a16c032dd0c
Victor Pakholkov <VictorPakholkov@outlook.com>
2024-04-27 00:36:48 +0300

    Create draw.py

---
+++ utils/draw.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+
+def draw_precision_recall(pr_data: list, class_idx: Optional[int]=None):
+	p = [data['precision'] for data in pr_data]
+	r = [data['recall'] for data in pr_data]
+
+	plt.plot(r, p, 'o-', color='r')
+	plt.xlabel("Recall")
+	plt.ylabel("Precision")
+
+	if class_idx is not None:
+		plt.title(categories[class_idx])
+
+	plt.show()
